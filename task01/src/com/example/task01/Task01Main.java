bc4143195cbdfe1529ee74a707441e10eb74650b
Artem Solovyov <artemsolovev121@gmail.com>
2024-11-28 18:41:55 +0500

    Соловьев Артём. Задание 01

--- task01/src/com/example/task01/Task01Main.java
+++ task01/src/com/example/task01/Task01Main.java
@@ -5,9 +5,6 @@ import java.io.InputStream;
 
 public class Task01Main {
     public static void main(String[] args) throws IOException {
-        //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-
         /*
         System.out.println(checkSumOfStream(new ByteArrayInputStream(new byte[]{0x33, 0x45, 0x01})));
         */
@@ -15,7 +12,19 @@ public class Task01Main {
     }
 
     public static int checkSumOfStream(InputStream inputStream) throws IOException {
-        // your implementation here
-        return 0;
+        if (inputStream == null) {
+            throw new IllegalArgumentException();
+        }
+
+        int b = inputStream.read();
+        int checkSum = 0;
+
+        while (b > -1) {
+            checkSum = Integer.rotateLeft(checkSum, 1) ^ b;
+            b = inputStream.read();
+        }
+
+
+        return checkSum;
     }
 }
