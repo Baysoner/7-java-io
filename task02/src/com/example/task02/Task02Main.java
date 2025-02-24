aa53ee576ca3b505f44e5ca34c40b4a81bcea9f6
Artem Solovyov <artemsolovev121@gmail.com>
2025-02-24 14:37:41 +0500

    Соловьев Артём. Задания 2-4

--- task02/src/com/example/task02/Task02Main.java
+++ task02/src/com/example/task02/Task02Main.java
@@ -1,6 +1,9 @@
 package com.example.task02;
 
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintStream;
 
 public class Task02Main {
     public static void main(String[] args) throws IOException {
@@ -9,5 +12,20 @@ public class Task02Main {
         // - направить стандартный вывод программы в файл output.test
         // - запустить программу
         // - и сравнить получившийся файл output.test с expected.test
+
+        InputStream inputStream = System.in;
+        int prev = inputStream.read();
+        int curr = inputStream.read();
+
+        while (prev != -1) {
+            if (prev != 13 || curr != 10) {
+                System.out.write(prev);
+            }
+            prev = curr;
+            curr = inputStream.read();
+        }
+        System.out.flush();
     }
+
+
 }
